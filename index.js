50f1e4dbe8702ee4ecaab71f5218ff38029bcbdc
kopihannan <kopihannan3@gmail.com>
2022-11-24 01:54:22 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.risshmy.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
